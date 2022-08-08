7f5fa731c38c8f6b5b8ce2a3195296eb41e9e887
Maxim Pavlov <ifelseapps@gmail.com>
2022-08-08 23:31:14 +0300

    Генерация статики

--- scripts/start.js
+++ scripts/start.js
@@ -1,3 +1,5 @@
+const Eleventy = require('@11ty/eleventy');
+const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
 const { fetchContent } = require('./helpers');
@@ -7,4 +9,16 @@ dotenv.config();
 const repository = process.env.CONTENT_REPOSITORY;
 const folder = process.env.CONTENT_FOLDER;
 
-fetchContent({ repository, folder });
+(async function () {
+  // Получаем контент из удаленного репозитория
+  fetchContent({ repository, folder });
+
+  // Генерируем статику
+  const elev = new Eleventy(folder);
+  await elev.write();
+
+  // Запускаем сервер для разработки
+  const app = express();
+  app.use('/', express.static(path.resolve(__dirname, '../_site')));
+  app.listen(3000);
+})();
