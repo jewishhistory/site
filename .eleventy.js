d8db4f23eabc0151a45cfb391492c8c3ac8465ad
Maxim Pavlov <ifelseapps@gmail.com>
2022-10-27 21:05:49 +0300

    Разметка шапки

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,51 @@
+const Image = require('@11ty/eleventy-img');
+const { format } = require('date-fns');
+const ruLocale = require('date-fns/locale/ru');
+const markdownIt = require('markdown-it');
+const markdownItAnchor = require('markdown-it-anchor');
+const htmlmin = require('html-minifier');
+const path = require('path');
+
 module.exports = (config) => {
-  // config.addPassthroughCopy('src/styles');
+  config.setDataDeepMerge(true);
+
+  config.setLiquidOptions({
+    strictFilters: false,
+  });
+
+  const markdownItOptions = {
+    html: true,
+  };
+
+  const markdownLib = markdownIt(markdownItOptions).use(
+    markdownItAnchor,
+    {
+      permalink: markdownItAnchor.permalink.headerLink()
+    }
+  );
+
+  config.setLibrary('md', markdownLib);
+
+  config.addTransform("htmlmin", function(content, outputPath) {
+    // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+    if( outputPath && outputPath.endsWith(".html") ) {
+      return htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      });
+    }
+
+    return content;
+  });
+
+
+  return {
+    dir: {
+      input: 'content',
+      includes: '../src/_includes',
+      layouts: '../src/layouts',
+      markdownTemplateEngine: 'liquid'
+    }
+  };
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,43 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: {
-    styles: './src/styles/common.css'
+    styles: './src/styles/bundle.css'
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, '_site/assets'),
   },
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new CopyPlugin({
+      patterns: [{
+        from: 'src/pictures/*',
+        to: 'pictures/[name][ext]',
+      }]
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.css$/i,
         exclude: /node_modules/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ['postcss-preset-env']
+                ]
+              }
+            }
+          }
+        ]
       }
     ]
   }
